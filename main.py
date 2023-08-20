a7b0ee80a94ac2d0df62b4e11166a672f7c1bbd9
Nikita Gitvoskiy <workfornikita@mail.ru>
2023-08-20 21:16:09 +0300

    Add a Doc

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
+
 def attack(char_name: str, char_class: str) -> str:
+    """Count the damage from char."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -14,6 +16,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Count the def to char."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -23,6 +26,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Use the spec spell."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение'
                 f'«Выносливость {80 + 25}»')
@@ -35,6 +39,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Start the training."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +65,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class():
+    """User take a name and class."""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
@@ -96,4 +102,3 @@ if __name__ == '__main__':
     print(start_training(char_name, char_class)) 
 
 
-main()
